89889c2385e6d25b62fe58b591bf6b1b6f6acfeb
Sal7one <Alanazi.saleh.77@gmail.com>
2022-03-23 18:05:51 +0300

    support for telephone numbers

--- ksa_numbers.py
+++ ksa_numbers.py
@@ -3,14 +3,13 @@ import re
 
 # Function to check if copied text is a KSA phone number
 
-
 def Checknumber():
 
     # Paste the copied text from clipboard to a string
     text = pyperclip.paste()
 
     # Use Regex to filter phone numbers from the text string
-    filteredtext = re.findall("((\+)?(966(_| |-)?|0)5[0-9]{8})", text)
+    filteredtext = re.findall("((\+)?(966([_ \-])?|0)(5[0-9]{8}|1([1-9])[0-9]{7}))", text)
 
     # Create a new array to save the numbers
     numbers = []
@@ -23,7 +22,6 @@ def Checknumber():
     result = '\n'.join(numbers)
     pyperclip.copy(result)
     print("Done. Paste the numbers anywehre you want!")
-
 
 # Call the function
 Checknumber()
